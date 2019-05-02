506ca69737f3409e7aba98afe7fa9e322e992f59
Janek Laudan <laudan@tu-berlin.de>
2019-05-02 14:58:50 +0200

    Create a create demand class

---
+++ src/test/java/org/matsim/class2019/demand/CreateDemandTest.java
@@ -0,0 +1,30 @@
+package org.matsim.class2019.demand;
+
+import org.junit.Test;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertNotNull;
+
+public class CreateDemandTest {
+
+	@Test
+	public void test() {
+		CreateDemand creator = new CreateDemand(Paths.get("C:\\Users\\Janek\\Desktop\\ueberregionale-pendler.csv"),
+				Paths.get("C:\\Users\\Janek\\Desktop\\regionale-pendler.csv"),
+				Paths.get("C:\\Users\\Janek\\Desktop\\thueringen-kreise.shp"),
+				Paths.get("C:\\Users\\Janek\\Desktop\\landcover.shp"));
+
+		creator.create();
+
+		Population result = creator.getPopulation();
+
+		assertNotNull(result);
+		System.out.println(result.getPersons().size());
+
+		new PopulationWriter(result).write(Paths.get("C:\\Users\\Janek\\Desktop\\population.xml.gz").toString());
+	}
+
+}
